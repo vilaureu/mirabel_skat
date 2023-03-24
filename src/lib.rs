ac5826cad0f4c625406bd82b2f89dc000b257262
vilaureu <vilaureu@users.noreply.github.com>
2023-03-24 10:26:30 +0100

    implement bidding game state

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,17 @@
 
 mod structures;
 
-use std::fmt::{self, Display, Write};
+use std::{
+    fmt::{self, Display, Write},
+    vec,
+};
 
 use mirabel::{
     cstr,
     error::{Error, ErrorCode, Result},
     game::{
-        player_id, semver, GameFeatures, GameMethods, Metadata, MoveCode, MoveData, PLAYER_RAND,
+        move_code, player_id, semver, GameFeatures, GameMethods, Metadata, MoveCode, MoveData,
+        PLAYER_RAND,
     },
     game_init::GameInit,
     plugin_get_game_methods, MoveDataSync,
@@ -21,7 +25,7 @@ use structures::{Card, CardStruct, Player};
 
 use crate::structures::OptCard;
 
-#[derive(Clone, Copy, Debug, Default)]
+#[derive(Clone, Debug, Default)]
 enum GameState {
     #[default]
     Dealing,
@@ -32,10 +36,13 @@ enum GameState {
     },
     Declaring,
     Playing,
+    // FIXME: Replace with fixed-size array.
+    Finished(Vec<Player>),
 }
 
 impl GameState {
     const MINIMUM_BID: u16 = 18;
+    const MAXIMUM_BID: u16 = 264;
 }
 
 impl Display for GameState {
@@ -52,6 +59,19 @@ impl Display for GameState {
             }
             GameState::Declaring => todo!(),
             GameState::Playing => todo!(),
+            GameState::Finished(players) => {
+                if players.is_empty() {
+                    write!(f, "draw")
+                } else {
+                    write!(
+                        f,
+                        "{} won",
+                        players
+                            .iter()
+                            .fold("".to_string(), |a, b| format!("{a} and {b}"))
+                    )
+                }
+            }
         }
     }
 }
@@ -72,7 +92,9 @@ enum BiddingState {
 }
 
 impl BiddingState {
-    /// Returns true when `self` represents a respond to a call.
+    /// Returns `true` when `self` represents a respond to a call.
+    ///
+    /// This also returns `true` in the [`Self::Forehand`] case.
     fn respond(&self) -> bool {
         match self {
             Self::MiddleCallsFore => false,
@@ -81,7 +103,7 @@ impl BiddingState {
             Self::ForeRespondsRear => true,
             Self::RearCallsMiddle => false,
             Self::MiddleRespondsRear => true,
-            Self::Forehand => false,
+            Self::Forehand => true,
         }
     }
 
@@ -113,6 +135,32 @@ impl BiddingState {
             Self::Forehand => panic!("the forehand is the only one left bidding"),
         }
     }
+
+    /// Evaluate next state after [`Self::source`] `passed` or not.
+    fn next(&self, passed: bool, any_bid: bool) -> BiddingResult {
+        if passed {
+            match self {
+                Self::MiddleCallsFore => BiddingResult::Continue(Self::RearCallsFore),
+                Self::ForeRespondsMiddle => BiddingResult::Continue(Self::RearCallsMiddle),
+                Self::RearCallsFore if any_bid => BiddingResult::Finished(Player::Forehand),
+                Self::RearCallsFore => BiddingResult::Continue(Self::Forehand),
+                Self::ForeRespondsRear => BiddingResult::Finished(Player::Rearhand),
+                Self::RearCallsMiddle => BiddingResult::Finished(Player::Middlehand),
+                Self::MiddleRespondsRear => BiddingResult::Finished(Player::Rearhand),
+                Self::Forehand => BiddingResult::Draw,
+            }
+        } else {
+            match self {
+                Self::MiddleCallsFore => BiddingResult::Continue(Self::ForeRespondsMiddle),
+                Self::ForeRespondsMiddle => BiddingResult::Continue(Self::MiddleCallsFore),
+                Self::RearCallsFore => BiddingResult::Continue(Self::ForeRespondsRear),
+                Self::ForeRespondsRear => BiddingResult::Continue(Self::RearCallsFore),
+                Self::RearCallsMiddle => BiddingResult::Continue(Self::MiddleRespondsRear),
+                Self::MiddleRespondsRear => BiddingResult::Continue(Self::RearCallsMiddle),
+                Self::Forehand => BiddingResult::Finished(Player::Forehand),
+            }
+        }
+    }
 }
 
 impl Display for BiddingState {
@@ -133,6 +181,15 @@ impl Display for BiddingState {
             )
         }
     }
+}
+
+enum BiddingResult {
+    /// Bidding continues.
+    Continue(BiddingState),
+    /// Bidding finished with [`Player`] becoming declarer.
+    Finished(Player),
+    /// All passed.
+    Draw,
 }
 
 #[derive(Clone, Debug, Default)]
@@ -189,12 +246,10 @@ impl GameMethods for Skat {
     fn players_to_move(&mut self, players: &mut Vec<player_id>) -> Result<()> {
         players.push(match self.state {
             GameState::Dealing => PLAYER_RAND,
-            GameState::Bidding { bid, state } => {
-                // TODO
-                Player::Middlehand.into()
-            }
+            GameState::Bidding { bid: _, state } => state.source().into(),
             GameState::Declaring => todo!(),
             GameState::Playing => todo!(),
+            GameState::Finished(_) => todo!(),
         });
         Ok(())
     }
@@ -206,9 +261,23 @@ impl GameMethods for Skat {
                     moves.push(OptCard::from(card).into())
                 }
             }
-            GameState::Bidding { bid, state } => todo!(),
+            GameState::Bidding { bid, state } => {
+                // 0 means passing.
+                moves.push(0.into());
+                if state.respond() {
+                    // 1 means accepting.
+                    moves.push(1.into());
+                } else {
+                    moves.extend(
+                        (bid.saturating_add(1)..=GameState::MAXIMUM_BID)
+                            .map(move_code::from)
+                            .map(MoveCode::from),
+                    );
+                }
+            }
             GameState::Declaring => todo!(),
             GameState::Playing => todo!(),
+            GameState::Finished(_) => todo!(),
         }
 
         Ok(())
@@ -219,15 +288,32 @@ impl GameMethods for Skat {
     /// Examples for dealing cards: `10S` for _10 of spades_ or `?` for a hidden
     /// action.
     fn get_move_data(&mut self, _player: player_id, string: &str) -> Result<Self::Move> {
-        Ok(match self.state {
+        let string = string.trim();
+        match self.state {
             GameState::Dealing => {
                 let card: OptCard = string.parse()?;
-                card.into()
+                Ok(card.into())
             }
-            GameState::Bidding { bid, state } => todo!(),
+            GameState::Bidding { bid: _, state: _ } => {
+                if string.eq_ignore_ascii_case("pass") {
+                    Ok(0.into())
+                } else if string.eq_ignore_ascii_case("accept")
+                    || string.eq_ignore_ascii_case("yes")
+                {
+                    Ok(1.into())
+                } else {
+                    string.parse().map(move_code::into).map_err(|e| {
+                        Error::new_dynamic(
+                            ErrorCode::InvalidInput,
+                            format!("failed to parse move as a valid number: {e}"),
+                        )
+                    })
+                }
+            }
             GameState::Declaring => todo!(),
             GameState::Playing => todo!(),
-        })
+            GameState::Finished(_) => todo!(),
+        }
     }
 
     fn get_move_str(
@@ -239,12 +325,25 @@ impl GameMethods for Skat {
         match self.state {
             GameState::Dealing => {
                 let card: OptCard = mov.md.try_into()?;
-                write!(str_buf, "{card}").expect("writing card action move failed");
+                write!(str_buf, "{card}")
             }
-            GameState::Bidding { bid, state } => todo!(),
+            GameState::Bidding { bid: _, state: _ } => {
+                #[allow(clippy::assertions_on_constants)]
+                const _: () = assert!(1 < GameState::MAXIMUM_BID);
+
+                if mov.md == 0 {
+                    write!(str_buf, "pass")
+                } else if mov.md == 1 {
+                    write!(str_buf, "accept")
+                } else {
+                    write!(str_buf, "{}", mov.md)
+                }
+            }
             GameState::Declaring => todo!(),
             GameState::Playing => todo!(),
+            GameState::Finished(_) => todo!(),
         }
+        .expect("writing move failed");
         Ok(())
     }
 
@@ -267,9 +366,25 @@ impl GameMethods for Skat {
                     };
                 }
             }
-            GameState::Bidding { bid, state } => todo!(),
+            GameState::Bidding { bid, state } => {
+                let any_bid = *bid >= GameState::MINIMUM_BID;
+                let next = match mov.md {
+                    0 => state.next(true, any_bid),
+                    1 => state.next(false, any_bid),
+                    m => {
+                        *bid = m.try_into().expect("bid overflowed");
+                        state.next(false, any_bid)
+                    }
+                };
+                match next {
+                    BiddingResult::Continue(s) => *state = s,
+                    BiddingResult::Finished(_) => todo!(),
+                    BiddingResult::Draw => self.state = GameState::Finished(Default::default()),
+                }
+            }
             GameState::Declaring => todo!(),
             GameState::Playing => todo!(),
+            GameState::Finished(_) => todo!(),
         }
 
         Ok(())
@@ -302,9 +417,29 @@ impl GameMethods for Skat {
                     }
                 }
             }
-            GameState::Bidding { bid, state } => todo!(),
+            GameState::Bidding { bid, state } => {
+                if Player::try_from(player) != Ok(state.source()) {
+                    return Err(Error::new_static(
+                        ErrorCode::InvalidPlayer,
+                        "player is currently not at turn while bidding\0",
+                    ));
+                }
+                if state.respond() {
+                    if mov.md > 1 {
+                        return Err(Error::new_static(
+                            ErrorCode::InvalidMove,
+                            "invalid bidding response\0",
+                        ));
+                    }
+                } else if mov.md != 0
+                    && (mov.md <= (*bid).into() || mov.md > GameState::MAXIMUM_BID.into())
+                {
+                    return Err(Error::new_static(ErrorCode::InvalidMove, "invalid bid\0"));
+                }
+            }
             GameState::Declaring => todo!(),
             GameState::Playing => todo!(),
+            GameState::Finished(_) => todo!(),
         }
 
         Ok(())
@@ -355,6 +490,7 @@ impl GameMethods for Skat {
             GameState::Bidding { bid, state } => todo!(),
             GameState::Declaring => todo!(),
             GameState::Playing => todo!(),
+            GameState::Finished(_) => todo!(),
         }
     }
 
